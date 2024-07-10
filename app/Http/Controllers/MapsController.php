7c40c29d149c6268b7e342feaacd02c26e8e501c
Juan David Ramírez <david.ramirez@datint.co>
2024-07-10 12:35:03 -0500

    pruba # 2222

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\puntos_gps;
 
 class MapsController extends Controller
-{//++++++++++prueba
+{//++++++++++prueba+//++++++++++++++++****
     public function buildArray()
     {
         $arc = fopen(base_path('puntos_gps.txt'), "r");
